b364d25eec124a40108fcfe1410882f3bd536129
Kayla Cross <kac288@georgetown.edu>
2018-09-15 16:51:25 +0000

    automatically backed up by learn

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,3 +1,19 @@
 class FiguresController < ApplicationController
-  # add controller methods
+
+  get '/figures' do
+
+  end
+
+  get '/figures/new' do
+
+  end
+
+  get '/figures/:id' do
+
+  end
+
+  get '/figures/:id/edit' do
+
+  end
+
 end
