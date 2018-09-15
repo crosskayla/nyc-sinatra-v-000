b5b5eea8f18d8b23f27d11d2bf07f2c72fb4cc87
Kayla Cross <kac288@georgetown.edu>
2018-09-15 16:52:21 +0000

    automatically backed up by learn

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,15 +1,15 @@
 class FiguresController < ApplicationController
 
   get '/figures' do
-
+    erb :'/figures/index'
   end
 
   get '/figures/new' do
-
+    erb :'/figures/new'
   end
 
   get '/figures/:id' do
-
+    erb :'/figures/show'
   end
 
   get '/figures/:id/edit' do
