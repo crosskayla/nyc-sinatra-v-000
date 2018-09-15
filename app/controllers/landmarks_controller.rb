eec163ed6f6551127d14261521367cc3d37cea9b
Kayla Cross <kac288@georgetown.edu>
2018-09-15 17:37:31 +0000

    automatically backed up by learn

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,11 +1,16 @@
 class LandmarksController < ApplicationController
 
   get '/landmarks' do
-    
+
   end
 
   get '/landmarks/new' do
-    
+
   end
-  
+
+  get '/landmarks/:id' do
+    @landmark = Landmark.find(params[:id])
+    erb :'/landmarks/show'
+  end
+
 end
